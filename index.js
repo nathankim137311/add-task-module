1605c6886811b6520efe9fd10fc2a47341297ff6
Kim <nathanjkim@student.cascadia.edu>
2021-10-18 18:51:20 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+console.log('hello everybody!');
+const taskBtn = document.getElementById('task-btn');
+const closeBtn = document.getElementById('close-btn');
+const addBtn = document.getElementById('add-btn'); 
+const taskList = document.getElementById('task-list');
+// task library 
+let myTasks = [];
+// input elements
+const titleInput = document.getElementById('title-input');
+const priorityInput = document.getElementById('priority-input'); 
+
+class Task {
+    constructor() {
+        this.title = titleInput.value;
+        this.priority = priorityInput.value; 
+    }
+}
+
+//  opens pop-up window 
+taskBtn.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    openForm(); 
+}); 
+
+// closes pop-up window 
+closeBtn.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    closeForm(); 
+});
+
+// adds task to task container 
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    // add task to container 
+    addTask(); 
+    createTask(myTasks.at(-1)); // new at() method selects last item of array
+    closeForm(); 
+});
+
+// adds task and saves object in local storage
+function addTask() {
+    const task = new Task(); 
+    myTasks.push(task); 
+    saveLocal(); 
+    console.log(myTasks); 
+}
+
+function createTask(task) {
+    const taskItems = document.createElement('li'); 
+    const taskTitle = document.createElement('h3'); 
+    const taskPriority = document.createElement('p');
+    taskTitle.textContent = task.title;
+    taskPriority.textContent = task.priority; 
+    taskItems.append(taskTitle, taskPriority); 
+    taskList.appendChild(taskItems);
+}
+
+// save to local storage 
+function saveLocal() {
+    localStorage.setItem('tasks', JSON.stringify(myTasks));
+}
+
+if(localStorage.getItem('tasks') === null) {
+    myTasks = []; 
+} else {
+    const tasksFromStorage = JSON.parse(localStorage.getItem('tasks')); 
+    myTasks = tasksFromStorage; 
+    for(let i = 0; i < myTasks.length; i++) {
+        createTask(myTasks[i]);
+    }
+}
+
+function openForm() {
+    document.getElementById('myForm').style.display = 'block'; 
+}
+
+function closeForm() {
+    document.getElementById('myForm').style.display = 'none';
+}
