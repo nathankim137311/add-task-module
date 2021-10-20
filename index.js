ccc091f1b1873f8d30273434bf9e526a31cd3b51
Kim <nathanjkim@student.cascadia.edu>
2021-10-19 17:20:38 -0700

    create project caategories whenever a user inputs anything in the projects input

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const taskList = document.getElementById('task-list');
 let myTasks = [];
 // input elements
 const titleInput = document.getElementById('title-input');
+const projectInput = document.getElementById('project-input'); 
 const priorityInput = document.getElementById('priority-input'); 
 
 class Task {
     constructor() {
         this.title = titleInput.value;
+        this.project = projectInput.value; 
         this.priority = priorityInput.value; 
     }
 }
@@ -45,29 +47,40 @@ function addTask() {
     console.log(myTasks); 
 }
 
+// creates task item 
 function createTask(task) {
-        const taskItems = document.createElement('li'); 
-        taskItems.classList.add('task-items');
-        taskItems.setAttribute('id', myTasks.indexOf(task))
-        const taskTitle = document.createElement('h3'); 
-        taskTitle.textContent = task.title;
-        const taskPriority = document.createElement('p');
-        taskPriority.textContent = task.priority; 
-        const trashBtn = document.createElement('button'); 
-        trashBtn.classList.add('trash-btn'); 
-        trashBtn.textContent = 'delete'; 
-        trashBtn.addEventListener('click', (e)=>{
-            const taskPosition = myTasks.indexOf(task); 
-            myTasks.splice(taskPosition, 1); 
-            taskItems.remove(); 
-            saveLocal(); 
-        }); 
-        taskItems.append(taskTitle, taskPriority, trashBtn); 
-        taskList.appendChild(taskItems);
+    const taskItems = document.createElement('li'); 
+    taskItems.classList.add('task-items');
+    taskItems.setAttribute('id', myTasks.indexOf(task))
+    const taskTitle = document.createElement('h3'); 
+    taskTitle.textContent = task.title;
+    const taskPriority = document.createElement('p');
+    taskPriority.textContent = task.priority; 
+    const trashBtn = document.createElement('button'); 
+    trashBtn.classList.add('trash-btn'); 
+    trashBtn.textContent = 'delete'; 
+    trashBtn.addEventListener('click', (e)=>{
+        const taskPosition = myTasks.indexOf(task); 
+        myTasks.splice(taskPosition, 1); 
+        taskItems.remove(); 
+        saveLocal(); 
+    }); 
+    taskItems.append(taskTitle, taskPriority, trashBtn); 
+    taskList.appendChild(taskItems);
+    console.log(task.project);
+    addProject(task.project);
 }
 
-
-
+// adds project to project list 
+function addProject(project) {
+    const projectsList = document.getElementById('projects-list'); 
+    const projectsListItem = document.createElement('li'); 
+    const projectName = document.createElement('a');
+    projectName.setAttribute('href', '#');
+    projectName.textContent = project;  
+    projectsList.appendChild(projectsListItem); 
+    projectsListItem.appendChild(projectName); 
+}
 
 // save to local storage 
 function saveLocal() {
