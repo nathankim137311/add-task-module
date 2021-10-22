df05aef3f60d0af49c9d9906434a670645e26475
Kim <nathanjkim@student.cascadia.edu>
2021-10-21 20:45:57 -0700

    added Project class and rearranged export

--- taskObj.js
+++ taskObj.js
@@ -3,11 +3,15 @@ const titleInput = document.getElementById('title-input');
 const projectInput = document.getElementById('project-input'); 
 const priorityInput = document.getElementById('priority-input'); 
 // task object constructor 
-class Task {
+export default class Task {
     constructor() {
         this.title = titleInput.value;
         this.project = projectInput.value; 
         this.priority = priorityInput.value; 
     }
 }
-export default Task
+export class Project {
+    constructor() {
+        this.Project = projectInput.value; 
+    }
+}
