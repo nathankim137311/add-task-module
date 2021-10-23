f49950a58286fea0a6103d983baa07a84231443e
Kim <nathanjkim@student.cascadia.edu>
2021-10-23 15:02:45 -0700

    local storage saves and added functions that populate the page with local storage items

--- overhaul.js
+++ overhaul.js
@@ -8,7 +8,7 @@ const addBtn = document.getElementById('add-btn');
 export const taskList = document.getElementById('task-list');
 // task library 
 export let myTasks = [];
-const myProjects = [];
+let myProjects = [];
 
 // let unique = new UniqueArray(myTasks);
 // console.log(unique);
@@ -38,6 +38,8 @@ addBtn.addEventListener('click', (e) => {
     e.preventDefault(); 
     createTask(); 
     createProjects(); 
+    saveLocal('tasks', myTasks); 
+    saveLocal('projects', myProjects); 
 });
 
 // creates task 
@@ -46,9 +48,7 @@ function createTask() {
     // push object to array 
     pushObject(myTasks, task)
     // creates DOM elements 
-    createTaskDom(task); 
-    // sets object to local storage 
-    saveLocal('tasks', myTasks); 
+    createTaskDom(task);  
 }
 
 function pushObject(arr, obj) {
@@ -58,22 +58,10 @@ function pushObject(arr, obj) {
 // delegates commands 
 function createProjects() {
     const projectName = projectInput.value; 
-    //const projectNames = [];
-    //projectNames.push(projectName); 
-    // checkArray(projectName, projectName);
     checkItem(projectName); 
 }
 
-// loops over array and checks item
-/*
-function checkArray(arr, item) {
-    for(let i = 0; i < arr.length; i++) {
-        checkItem(item); 
-    }
-}
-*/
-
-// checks array for duplicates
+// checks array for duplicates and creates project list 
 function checkItem(item) {
     if(myProjects.indexOf(item) === -1) {
         myProjects.push(item);
@@ -86,25 +74,43 @@ function saveLocal(name, arr) {
     localStorage.setItem(name, JSON.stringify(arr)); 
 }
 
-// checks if tasks array is empty if it's not generate tasks 
-if(localStorage.getItem('tasks') === null) {
-    myTasks = []; 
-    myProjects = [];
-} else {
-    const tasksFromStorage = JSON.parse(localStorage.getItem('tasks')); 
-    myTasks = tasksFromStorage;
-    // creates from existing library 
+localStorageItems(); 
 
-    /*
-    for(let i = 0; i < myTasks.length; i++) {
-        createTask(myTasks[i]);
+// checks if tasks array is empty if it's not generate tasks 
+function localStorageItems() {
+    if(getItemsFromStorage('tasks') === null || getItemsFromStorage('projects') === null) {
+        myTasks = [];
+        myProjects = [];
+    } else {
+        const tasksFromStorage = getItemsFromStorage('tasks'); 
+        myTasks = tasksFromStorage;
+        console.log(myTasks); 
+        const projectsFromStorage = getItemsFromStorage('projects')
+        myProjects = projectsFromStorage; 
+        console.log(myProjects);
+        // creates from existing library 
+        createItemsFromStorage(myTasks); 
+        createItemsFromStorage(myProjects);
     }
-    for(let i = 0; i < myProjects.length; i++) {
-        createProjectList(myProjects[i]); 
-    }
-    */
 }
 
+function getItemsFromStorage(str) {
+    return JSON.parse(localStorage.getItem(str))
+}
+
+function createItemsFromStorage(arr) {
+    switch(arr) {
+        case myTasks:
+            for(let i = 0; i < arr.length; i++) {
+                createTaskDom(arr[i]); 
+            }
+            break;
+        case myProjects:
+            for(let i = 0; i < arr.length; i++) {
+                createProjectListDom(arr[i]); 
+            } 
+    }
+}
 /*
 function createProjectListDom(arr) {
     const projectsList = document.getElementById('projects-list');
